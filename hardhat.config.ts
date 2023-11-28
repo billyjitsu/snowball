360df0278e830aadf81a9d50ede45bcf8b683dbc
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-11-28 12:33:23 -0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,11 +39,29 @@ networks: {
     accounts: [`${process.env.PRIVATE_KEY}`],
     // gas: 200000000,
     // gasPrice: 100000000000,
-  }
+  },
+  mantle: {
+    url: "https://rpc.mantle.xyz", //mainnet
+    accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? ''],
+    },
+    mantleTest: {
+    url: "https://rpc.testnet.mantle.xyz", // testnet
+    accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? '']
+    }
 
 },
 etherscan: {
-apiKey: process.env.ETHERSCAN_API_KEY || ""
+apiKey: process.env.ETHERSCAN_API_KEY || "",
+customChains: [
+  {
+      network: "mantleTest",
+      chainId: 5001,
+      urls: {
+      apiURL: "https://explorer.testnet.mantle.xyz/api",
+      browserURL: "https://explorer.testnet.mantle.xyz"
+      }
+  }
+  ]
 },
 
 sourcify: {
