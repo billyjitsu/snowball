906b94493a844e89f8490a4f4659adada2b57d2e
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-12-10 21:15:10 -0800

    update attack script

--- scripts/zattack.js
+++ scripts/zattack.js
@@ -2,11 +2,22 @@ const ethers = require("ethers");
 const airnodeAdmin = require("@api3/airnode-admin");
 require("dotenv").config();
 const ABI = require("../artifacts/contracts/NFTAttack.sol/NFTAttack.json");
-const { airnodeAddress, airnodeXpub, endPointAddress, yourDeployedContractAddress, sponsorWallet } = require("./variables");
-const { latest } = require("@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time");
-
+const {
+  airnodeAddress,
+  airnodeXpub,
+  endPointAddress,
+  yourDeployedContractAddress,
+  sponsorWallet,
+} = require("./variables");
+const {
+  latest,
+} = require("@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time");
+const { resolve } = require("path");
 
 async function main() {
+
+  const attacker = "0xe2b8651bF50913057fF47FC4f02A8e12146083B8";
+  const victim = "0x9263bFf6ACCb60E83254E95220e7637465298171";
   // Connect to a provider (e.g., Infura, Alchemy)
   const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
   // Use your private key (keep this secure!)
@@ -17,128 +28,126 @@ async function main() {
   const contractABI = ABI.abi;
 
   // Create a contract instance
-  const contract = new ethers.Contract(yourDeployedContractAddress, contractABI, wallet);
-
-  console.log(
-    "Starting Attack..."
+  const contract = new ethers.Contract(
+    yourDeployedContractAddress,
+    contractABI,
+    wallet
   );
 
-  const receipt = await contract.throwSnowball('0x9263bFf6ACCb60E83254E95220e7637465298171');
+  console.log("Starting Attack...");
+
+  const receipt = await contract.throwSnowball("0x9263bFf6ACCb60E83254E95220e7637465298171");
+
+  // console.log("Hash of the transaction: ", receipt.hash);
+
+  let requestID;
+  // Listen for all events from the contract
+  contract.once("*", (event) => {
+    // filter out with the specific transaction hash
+    if (event.log.transactionHash === receipt.hash) {
+      requestID = event.args[0];
+      console.log("requestID: ", requestID);
+    }
+  });
 
   let txReceipt = await receipt.wait();
   if (txReceipt.status === 0) {
     throw new Error("Transaction failed");
   }
-  console.log(
-    "Request completed!"
-  );
-
+  console.log("Request completed successfully with request ID:", requestID);
   // console.log("Tnx logs: ", txReceipt.logs);
-
   // console.log("transaction receieipt: ", txReceipt.logsBloom);
   // console.log("transaction receieipt full: ", txReceipt);
 
-  const eventTopic = ethers.id('RequestUint256(bytes32)'); // Get the data hex string
-
+  console.log("Waiting for the request to be fulfilled...");
   
-  let rawlog = await provider.getLogs({
-    address: contract.address,
-    topics: [eventTopic],
-    fromBlock: txReceipt.blockNumber - 1,
-    toBlock: txReceipt.blockNumber
+  const response = await new Promise((resolve) => {
+    // Function to clean up event listeners
+    function cleanup() {
+      contract.removeAllListeners(contract.filters.MissedAttack(attacker, victim));
+      contract.removeAllListeners(contract.filters.SuccessfulAttack(attacker, victim, null, null));
+      contract.removeAllListeners(contract.filters.NFTBurned(victim, null, attacker));
+    }
+  
+    // Setting up listeners for each event
+    contract.once(contract.filters.MissedAttack(), (event) => {
+      console.log("Missed Attack");
+      console.log("Attacker: ", event.args[0]);
+      console.log("Victim: ", event.args[1]);
+      resolve(event);
+      cleanup();
+    });
+  
+    contract.once(contract.filters.SuccessfulAttack(), (event) => {
+      console.log("Successful Attack");
+      console.log("Attacker: ", event.args[0]);
+      console.log("Victim: ", event.args[1]);
+      console.log("Attack Power: ", event.args[2]);
+      console.log("Victim Health: ", event.args[3]);
+
+      resolve(event);
+      cleanup();
+    });
+  
+    contract.once(contract.filters.NFTBurned(), (event) => {
+      console.log("NFT Burned");
+      console.log("Victim: ", event.args[0]);
+      console.log("Token ID Burned: ", event.args[1]);
+      console.log("Attacker: ", event.args[2]);
+      resolve(event);
+      cleanup();
+    });
   });
- // console.log("rawlog: ", rawlog);
 
-  // async function processLogsWithInterface() {
-    // const abi = ABI.abi;
-    // const intrfc = new ethers.Interface(abi);
-
-    // rawlog.forEach((log) => {
-    //     console.log(`BEFORE PARSING:`);
-    //     console.debug(log);
-    //     console.log(`\n`);
-
-    //     console.log(`AFTER PARSING:`);
-    //     let parsedLog = intrfc.parseLog(log);
-    //     console.debug(parsedLog);
-    //     console.log('************************************************');
-    // })
-//}
-
-
-// //Working
-// let events = await contract.queryFilter('RequestUint256', txReceipt.blockNumber, txReceipt.blockNumber);
-
-//   console.log("Ethers version event:",events);
+//  console.log("response: ", response);
   
 
-//   // //first listen for the event
-//   // console.log("1st listen");
-//   contract.on("RequestUint256", (requestID, event) => {
-//     let info = {
-//       requestID: requestID,
-//       event: event,
-//     };
-//     console.log(JSON.stringify(info, null, 2));
-//     }
-//   );
+  /*
+Contract Listeners
+*/
 
-  // 2nd listen
-  // console.log("2nd listen");
-  // contract.on("*", (from, to, tokenId, event) => {
-  //   console.log(`${ from } => ${ to }: ${ tokenId }`)
+  // //first listen for the event
+  // console.log("1st listen");
+  // contract.once("RequestUint256", (requestID, event) => {
 
-  //     // Optionally, convenience method to stop listening
-  //    // event.removeListener();
+  //   console.log("requestID: ", requestID);
+  //   console.log("event: ", event);
+  //   }
+  // );
+
+  /* *************************** */
+
+  // Listen for all events from the contract
+  // console.log("Listening for the hashed event")
+  // contract.on('*', (event) => {
+  // // if (event.transactionHash === txResponse.hash) {
+  //     // This event is associated with the transaction hash we're interested in
+  //     // console.log("Event from our transaction:", event);
+
+  //     console.log("********************");
+  //     console.log("requestID: ", event.args[0]);
+
+  //     console.log("********************");
+  //     console.log("Event log breakdown(tx hash): ", event.log.transactionHash);
+
+  //   // }
   // });
 
-  // 3rd listen
-  // console.log("3rd listen");
-  // contract.on("*", (event) => { 
-  //   console.log(event.eventName, event.args, event.log); 
+  /* *************************** */
+  // //Working
+  // let events = await contract.queryFilter('RequestUint256', txReceipt.blockNumber, txReceipt.blockNumber);
+  //   console.log("Ethers version event:",events);
+
+  /* *************************** */
+  // const eventTopic = ethers.id('RequestUint256(bytes32)'); // Get the data hex string
+
+  // let rawlog = await provider.getLogs({
+  //   address: contract.address,
+  //   topics: [eventTopic],
+  //   fromBlock: txReceipt.blockNumber - 1,
+  //   toBlock: txReceipt.blockNumber
   // });
-
-
-
-  
-
-  //////
-//    // and read the logs once it gets confirmed to get the request ID
-//    console.log("Waiting for the request to be fulfilled...");
-
-//    const requestId = await new Promise((resolve) =>
-//    contract.once(requestRandomNumber.hash, (tx) => {
-//      // We want the log from RrpRequesterContract
-//      const log = tx.logs.find(
-//        (log) => log.address === contractAddress
-//      );
-//      const parsedLog = contract.interface.parseLog(log);
-//      resolve(parsedLog.args.requestId);
-//    })
-//  );
-//  console.log(`Transaction is confirmed, request ID is ${requestId}`);
-
-//  // Wait for the fulfillment transaction to be confirmed and read the logs to get the random number
-//  console.log("Waiting for the fulfillment transaction...");
-//  const log = await new Promise((resolve) =>
-//    hre.ethers.provider.once(
-//      RrpRequesterContract.filters.RequestFulfilled(requestId, null),
-//      resolve
-//    )
-//  );
-//  const parsedLog = RrpRequesterContract.interface.parseLog(log);
-//  const decodedData = parsedLog.args.response;
-//  console.log(
-//    `Fulfillment is confirmed, response is ${decodedData.toString()}`
-//  );
- /////////////
-
-
-
-
-
-
-  
+  // console.log("rawlog: ", rawlog);
 }
 
 main()
