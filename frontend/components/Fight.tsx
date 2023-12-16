3ca0cdf83c6fa789400fe54c55022e834fc0667f
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-12-15 19:26:39 -0800

    update site

--- frontend/components/Fight.tsx
+++ frontend/components/Fight.tsx
@@ -34,41 +34,17 @@ const Fight = () => {
     abi: Snowfight.abi,
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event: any) => {
     setDefenderAddress(event.target.value);
   };
 
   // Optional: Validate the input as Ethereum address or ENS name
-  const isValidAddress = (input) => {
+  const isValidAddress = (input: any) => {
     // Basic validation logic (can be expanded)
     return input.length === 42 || input.endsWith(".eth");
   };
 
-  // //Chose racer number
-  // const handlePredictionInputChange = (
-  //   event: React.ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   setPredictionInput(event.target.value);
-  //   //setYoutubeId(event.target.value);
-  // };
-
-  // //Choose amount of eth
-  // const handleETHInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setEthAmount(Number(event.target.value));
-  //   //setYoutubeId(event.target.value);
-  // };
-
-  // //Choose amount of Ape Insurance
-  // const handleApeInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setApeAmount(Number(event.target.value));
-  //   //setYoutubeId(event.target.value);
-  // };
-
-  const handleSubmit = () => {
-    // Handle the submission of the YouTube ID here
-    // sendRequest();
-    // console.log("Value:", inputValue);
-  };
+  
 
   const mintTheNFT = async (nftNum: number) => {
     try {
@@ -107,6 +83,21 @@ const Fight = () => {
     }
   };
 
+  const checkOpponentNFT = async () => { 
+    try {
+      const hasMinted = await readContract({
+        address: contractAddress,
+        abi: Snowfight.abi,
+        functionName: "checkIfUserNFT",
+        args: [defenderAddress],
+      });
+      // console.log("Has Minted:", hasMinted);
+      return hasMinted;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     interface MintedData {
       name: string;
@@ -143,102 +134,107 @@ const Fight = () => {
     <div className="bg-black h-screen w-full ">
       <>
         {!loading && (
-          <div className="flex flex-col md:flex-row px-5 justify-center lg:mr-16 h-screen w-full">
-            <div className="relative flex w-full h-screen content-center items-center justify-center md:h-screen z-10 bg-gradient-to-b from-black  to-slate-300">
-              <div className="container relative mx-auto p-16 md:p-0">
-                <div className="flex flex-col  items-center justify-center -mt-6 md:mt-0 sm:-ml-0 md:-ml-12">
-                  <div className="text-center md:text-left md:ml-16 space-x-2 space-y-2">
-                    {!loading && !minted && (
-                      <>
-                        <h1 className="text-3xl md:text-5xl font-bold text-center text-white ">
-                          Ready your snowball
-                        </h1>
-                        {/* <p className="text-white text-center text-xl">
+          <div className="flex flex-col md:flex-row justify-center h-screen w-full">
+            <div className="relative flex w-full h-screen content-center items-center justify-center z-10 bg-gradient-to-b from-black to-slate-300">
+              <div className="flex flex-col  items-center justify-center">
+                <div className="text-center md:text-left space-x-2 space-y-2">
+                  {!loading && !minted && (
+                    <>
+                      <h1 className="text-3xl md:text-5xl font-bold text-center text-white ">
+                        Ready your snowball
+                      </h1>
+                      {/* <p className="text-white text-center text-xl">
                           May the odds be in your favor
                         </p> */}
 
-                        {/* Image  */}
-                        <div className="container flex flex-row mx-auto p-4">
-                          {/* Attacker */}
-                          {nftData && (
-                            <div className="nft-data-container flex flex-col items-center">
-                              <img
-                                src={nftData.imageURI}
-                                alt={nftData.name}
-                                className="w-1/2 h-auto"
-                              />
-                              <div className="nft-data-text mt-1 text-center">
-                                <h3 className="text-2xl font-bold">
-                                  {nftData.name}
-                                </h3>
-                                <p className="text-sm">
-                                  Health: {nftData.hp.toString()}
-                                </p>
-                                <p className="text-sm">
-                                  Attack: {nftData.attackDamage.toString()}
-                                </p>
-                                <p className="text-sm">
-                                  Defense: {nftData.defense.toString()}
-                                </p>
-                                <p className="text-sm">
-                                  Evade: {nftData.evade.toString()}
-                                </p>
-                              </div>
-                            </div>
-                          )}
-                          {/* Defender */}
-                          <div>
-                            <input
-                              type="text"
-                              value={defenderAddress}
-                              onChange={handleChange}
-                              placeholder="Enter Ethereum Address or ENS Name"
-                              className="w-full p-2 border border-gray-300 rounded"
+                      {/* Image  */}
+                      <div className="container flex flex-row mx-auto p-4">
+                        {/* Attacker */}
+                        {nftData && (
+                          <div className="nft-data-container flex flex-col items-center">
+                            <img
+                              src={nftData.imageURI}
+                              alt={nftData.name}
+                              className="w-1/2 h-auto"
                             />
-                            {defenderAddress && !isValidAddress(defenderAddress) && (
+                            <div className="nft-data-text mt-1 text-center">
+                              <h3 className="text-2xl font-bold">
+                                {nftData.name}
+                              </h3>
+                              <p className="text-sm">
+                                Health: {nftData.hp.toString()}
+                              </p>
+                              <p className="text-sm">
+                                Attack: {nftData.attackDamage.toString()}
+                              </p>
+                              <p className="text-sm">
+                                Defense: {nftData.defense.toString()}
+                              </p>
+                              <p className="text-sm">
+                                Evade: {nftData.evade.toString()}
+                              </p>
+                            </div>
+                          </div>
+                        )}
+
+                        {/* VS */}
+                        <div className="flex flex-col items-center justify-center">
+                          <h1 className="text-6xl font-bold text-white ">VS</h1>
+                        </div>
+
+                        {/* Defender */}
+                        <div className="flex flex-col space-y-2 md:ml-8 w-full md:w-1/2 items-center justify-center mb-24">
+                          <input
+                            type="text"
+                            value={defenderAddress}
+                            onChange={handleChange}
+                            placeholder="Enter Ethereum Address or ENS Name"
+                            className="w-full p-2 border border-gray-300 rounded"
+                          />
+                          {defenderAddress &&
+                            !isValidAddress(defenderAddress) && (
                               <p className="text-red-500">
                                 Invalid address or ENS name
                               </p>
                             )}
-                          </div>
-                        </div>
-                      </>
-                    )}
-
-                    {minted && (
-                      <div
-                        className="max-w-xs m-auto bg-green-500 text-sm text-white rounded-md shadow-lg dark:bg-green-900"
-                        role="alert"
-                      >
-                        <div className="flex p-4">
-                          <span>Success. Click to close.</span>
-
-                          <div className="ml-auto">
-                            <button
-                              type="button"
-                              onClick={() => setMinted(false)}
-                              className="inline-flex flex-shrink-0 justify-center items-center h-4 w-4 rounded-md text-white/[.5] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-gray-600 transition-all text-sm dark:focus:ring-offset-gray-900 dark:focus:ring-gray-800"
-                            >
-                              <span className="sr-only">Close</span>
-                              <svg
-                                className="w-3.5 h-3.5"
-                                width="16"
-                                height="16"
-                                viewBox="0 0 16 16"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  d="M0.92524 0.687069C1.126 0.486219 1.39823 0.373377 1.68209 0.373377C1.96597 0.373377 2.2382 0.486219 2.43894 0.687069L8.10514 6.35813L13.7714 0.687069C13.8701 0.584748 13.9882 0.503105 14.1188 0.446962C14.2494 0.39082 14.3899 0.361248 14.5321 0.360026C14.6742 0.358783 14.8151 0.38589 14.9468 0.439762C15.0782 0.493633 15.1977 0.573197 15.2983 0.673783C15.3987 0.774389 15.4784 0.894026 15.5321 1.02568C15.5859 1.15736 15.6131 1.29845 15.6118 1.44071C15.6105 1.58297 15.5809 1.72357 15.5248 1.85428C15.4688 1.98499 15.3872 2.10324 15.2851 2.20206L9.61883 7.87312L15.2851 13.5441C15.4801 13.7462 15.588 14.0168 15.5854 14.2977C15.5831 14.5787 15.4705 14.8474 15.272 15.046C15.0735 15.2449 14.805 15.3574 14.5244 15.3599C14.2437 15.3623 13.9733 15.2543 13.7714 15.0591L8.10514 9.38812L2.43894 15.0591C2.23704 15.2543 1.96663 15.3623 1.68594 15.3599C1.40526 15.3574 1.13677 15.2449 0.938279 15.046C0.739807 14.8474 0.627232 14.5787 0.624791 14.2977C0.62235 14.0168 0.730236 13.7462 0.92524 13.5441L6.59144 7.87312L0.92524 2.20206C0.724562 2.00115 0.611816 1.72867 0.611816 1.44457C0.611816 1.16047 0.724562 0.887983 0.92524 0.687069Z"
-                                  fill="currentColor"
-                                />
-                              </svg>
-                            </button>
-                          </div>
                         </div>
                       </div>
-                    )}
-                  </div>
+                    </>
+                  )}
+
+                  {minted && (
+                    <div
+                      className="max-w-xs m-auto bg-green-500 text-sm text-white rounded-md shadow-lg dark:bg-green-900"
+                      role="alert"
+                    >
+                      <div className="flex p-4">
+                        <span>Success. Click to close.</span>
+
+                        <div className="ml-auto">
+                          <button
+                            type="button"
+                            onClick={() => setMinted(false)}
+                            className="inline-flex flex-shrink-0 justify-center items-center h-4 w-4 rounded-md text-white/[.5] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-gray-600 transition-all text-sm dark:focus:ring-offset-gray-900 dark:focus:ring-gray-800"
+                          >
+                            <span className="sr-only">Close</span>
+                            <svg
+                              className="w-3.5 h-3.5"
+                              width="16"
+                              height="16"
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M0.92524 0.687069C1.126 0.486219 1.39823 0.373377 1.68209 0.373377C1.96597 0.373377 2.2382 0.486219 2.43894 0.687069L8.10514 6.35813L13.7714 0.687069C13.8701 0.584748 13.9882 0.503105 14.1188 0.446962C14.2494 0.39082 14.3899 0.361248 14.5321 0.360026C14.6742 0.358783 14.8151 0.38589 14.9468 0.439762C15.0782 0.493633 15.1977 0.573197 15.2983 0.673783C15.3987 0.774389 15.4784 0.894026 15.5321 1.02568C15.5859 1.15736 15.6131 1.29845 15.6118 1.44071C15.6105 1.58297 15.5809 1.72357 15.5248 1.85428C15.4688 1.98499 15.3872 2.10324 15.2851 2.20206L9.61883 7.87312L15.2851 13.5441C15.4801 13.7462 15.588 14.0168 15.5854 14.2977C15.5831 14.5787 15.4705 14.8474 15.272 15.046C15.0735 15.2449 14.805 15.3574 14.5244 15.3599C14.2437 15.3623 13.9733 15.2543 13.7714 15.0591L8.10514 9.38812L2.43894 15.0591C2.23704 15.2543 1.96663 15.3623 1.68594 15.3599C1.40526 15.3574 1.13677 15.2449 0.938279 15.046C0.739807 14.8474 0.627232 14.5787 0.624791 14.2977C0.62235 14.0168 0.730236 13.7462 0.92524 13.5441L6.59144 7.87312L0.92524 2.20206C0.724562 2.00115 0.611816 1.72867 0.611816 1.44457C0.611816 1.16047 0.724562 0.887983 0.92524 0.687069Z"
+                                fill="currentColor"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
