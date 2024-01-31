0739a51a81926efb85acc6e597c020b15b124ee3
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2024-01-30 22:45:57 -0800

    update sponsor and contract

--- scripts/variables.js
+++ scripts/variables.js
@@ -2,8 +2,8 @@ const airnodeAddress = '0x6238772544f029ecaBfDED4300f13A3c4FE84E1D';
 const airnodeXpub = 'xpub6CuDdF9zdWTRuGybJPuZUGnU4suZowMmgu15bjFZT2o6PUtk4Lo78KGJUGBobz3pPKRaN9sLxzj21CMe6StP3zUsd8tWEJPgZBesYBMY7Wo';
 const endPointAddress = '0x94555f83f1addda23fdaa7c74f27ce2b764ed5cc430c66f5ff1bcf39d583da36';
 
-const yourDeployedContractAddress = '0xb8EAa40a7976474a47bB48291FE569f383069FBc';
+const yourDeployedContractAddress = '0x32a13fe01300Be6ED5471a4F8228A68F9b907269';
 
-const sponsorWallet = '0x8885403a5E1882b2498b8444B7029E067F9d17B3';
+const sponsorWallet = '0xd6EDd1f8b2D6f395bb87eF3Da840f05AF366513A';
 
 module.exports = { airnodeAddress, airnodeXpub, endPointAddress, yourDeployedContractAddress, sponsorWallet };
