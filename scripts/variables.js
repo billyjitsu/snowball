fd060411d0eb9c76e2057b1a522032768c95caf8
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2024-02-09 19:52:07 -0800

    up

--- scripts/variables.js
+++ scripts/variables.js
@@ -2,7 +2,7 @@ const airnodeAddress = '0x6238772544f029ecaBfDED4300f13A3c4FE84E1D';
 const airnodeXpub = 'xpub6CuDdF9zdWTRuGybJPuZUGnU4suZowMmgu15bjFZT2o6PUtk4Lo78KGJUGBobz3pPKRaN9sLxzj21CMe6StP3zUsd8tWEJPgZBesYBMY7Wo';
 const endPointAddress = '0x94555f83f1addda23fdaa7c74f27ce2b764ed5cc430c66f5ff1bcf39d583da36';
                                     //Fill in with YOUR deployed contract address
-const yourDeployedContractAddress = '0xdA976c89DbC30046Bb093dfa1E457AB1A51305ED';
+const yourDeployedContractAddress = '0x74b17BbA1F94141552F9697F16f29fc1Edb1AEf7';
                       //Fill in with YOUR generated sponsor wallet address
 const sponsorWallet = '0x86c0Da0D8A3220D90bb71CCebc980A3b9236C851';
 
